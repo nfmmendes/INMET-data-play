a839d9210440ed44bd2dd7ac19bc5f1872ed5e69
nfmmendes <nilson.fmmendes@outlook.com>
2024-10-25 18:49:18 +0200

    Rename data column

--- main.py
+++ main.py
@@ -4,31 +4,33 @@ import matplotlib.pyplot as plt
 
 #### LE DADOS 2008
 df_ini = pd.read_csv("./2008/INMET_SE_MG_A552_SALINAS_01-01-2008_A_31-12-2008.CSV", sep=";",decimal=",", skiprows=8, encoding="ANSI")
+df_ini = df_ini.rename({df_ini.columns[0]: 'Data'}, axis='columns')
 
-
-df_ini = df_ini[["DATA (YYYY-MM-DD)",\
+df_ini = df_ini[["Data",\
         "HORA (UTC)",\
         "PRECIPITAÇÃO TOTAL, HORÁRIO (mm)",\
         "TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)",\
         "TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)",\
         "TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)"]]
 
-df_ini['DATA (YYYY-MM-DD)'] = df_ini['DATA (YYYY-MM-DD)'].astype('datetime64[ns]')
+df_ini['Data'] = df_ini['Data'].astype('datetime64[ns]')
 clean = df_ini[df_ini["TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"].between(-273, 100)]
-plt.plot([ f"{x.day}/{x.month}" for x in clean["DATA (YYYY-MM-DD)"]], clean["TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"].rolling(48).mean(), label="2008")
+plt.plot([ f"{x.day}/{x.month}" for x in clean["Data"]], clean["TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"].rolling(48).mean(), label="2008")
 
 
 ### LE DADOS 2015
 df_mid = pd.read_csv("./2015/INMET_SE_MG_A552_SALINAS_01-01-2015_A_31-12-2015.CSV", sep=";",decimal=",", skiprows=8, encoding="ANSI")
+df_mid = df_mid.rename({df_mid.columns[0]: 'Data'}, axis='columns')
 
-df_mid = df_mid[["DATA (YYYY-MM-DD)",\
+
+df_mid = df_mid[["Data",\
                 "HORA (UTC)",\
                 "PRECIPITAÇÃO TOTAL, HORÁRIO (mm)",\
                 "TEMPERATURA DO AR - BULBO SECO, HORARIA (°C)",\
                 "TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)",\
                 "TEMPERATURA MÍNIMA NA HORA ANT. (AUT) (°C)"]]
 
-df_mid['DATA (YYYY-MM-DD)'] = df_mid['DATA (YYYY-MM-DD)'].astype('datetime64[ns]')
+df_mid['Data'] = df_mid['Data'].astype('datetime64[ns]')
 clean = df_mid[df_mid["TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"].between(-273, 100)]
 #plt.plot([ f"{x.day}/{x.month}" for x in clean["DATA (YYYY-MM-DD)"]], clean["TEMPERATURA MÁXIMA NA HORA ANT. (AUT) (°C)"].rolling(48).mean(), label="2015")
 
